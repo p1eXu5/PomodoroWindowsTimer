e30e8da3649bd5b604bd9d3e1d2499c79e5abad2
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2024-04-21 17:12:16 +0300

    add animation om Timer on open drawer

---
+++ src/PomodoroWindowsTimer.WpfClient/UserControls/UIAssistant.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace PomodoroWindowsTimer.WpfClient.UserControls;
+
+public static class UIAssistant
+{
+    public static readonly DependencyProperty AreRunningTimePointsShownProperty =
+            DependencyProperty.RegisterAttached(
+          "AreRunningTimePointsShown",
+          typeof(bool),
+          typeof(UIAssistant),
+          new FrameworkPropertyMetadata(defaultValue: false)
+        );
+
+    public static bool GetAreRunningTimePointsShown(UIElement target) =>
+        (bool)target.GetValue(AreRunningTimePointsShownProperty);
+
+    public static void SetAreRunningTimePointsShown(UIElement target, bool value) =>
+        target.SetValue(AreRunningTimePointsShownProperty, value);
+}
