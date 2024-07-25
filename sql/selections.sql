7a11320f763cc8e2ad9c79c4501508b43c43edfe
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2024-07-25 11:26:12 +0300

    [ sql ] - add selections

--- sql/selections.sql
+++ sql/selections.sql
@@ -41,3 +41,39 @@ FROM work_event
 GROUP BY work_id
 HAVING work_id IN (1, 2, 3)
 ;
+
+WITH
+    work_last_event(work_id, last_event_created_at) AS (
+        SELECT
+              work_id
+            , MAX(created_at) AS last_event_created_at
+        FROM work_event
+        GROUP BY work_id
+    )
+SELECT
+      w.id
+    , w.number
+    , w.title
+    , w.created_at
+    , w.updated_at
+    , IFNULL(e.last_event_created_at, -1) AS last_event_created_at
+FROM work AS w
+LEFT JOIN work_last_event AS e ON e.work_id = w.id
+;
+
+WITH
+    work_last_event(work_id, last_event_created_at) AS (
+        SELECT
+              work_id
+            , MAX(created_at) AS last_event_created_at
+        FROM work_event
+        GROUP BY work_id
+    )
+SELECT
+      w.*
+    , IFNULL(e.last_event_created_at, -1) AS last_event_created_at
+FROM work AS w
+LEFT JOIN work_last_event AS e ON e.work_id = w.id
+WHERE w.number LIKE 'Text'
+    OR w.title LIKE 'Text'
+;
