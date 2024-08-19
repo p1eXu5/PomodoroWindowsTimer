893444cc5b5939beaf9bb7f247aa5c2fb5f9363c
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2024-08-19 10:51:34 +0300

    styles

--- src/PomodoroWindowsTimer.WpfClient/Behaviors/DialogEventHandlerBehavior.cs
+++ src/PomodoroWindowsTimer.WpfClient/Behaviors/DialogEventHandlerBehavior.cs
@@ -6,6 +6,11 @@ using Microsoft.Xaml.Behaviors;
 
 namespace PomodoroWindowsTimer.WpfClient.Behaviors;
 
+/// <summary>
+/// Applying to button. This button must invoke `materialDesign:DialogHost.OpenDialogCommand` and pass in command parameter
+/// dialog template content. This behavion sets <see cref="DialogOpenedEventHandler"/> and <see cref="DialogClosedEventHandler"/>
+/// with delegates to load dialog models and process ok and cancel commands.
+/// </summary>
 internal sealed class DialogEventHandlerBehavior : Behavior<Button>
 {
     private DialogOpenedEventHandler? _openedEventHandler;
@@ -17,6 +22,83 @@ internal sealed class DialogEventHandlerBehavior : Behavior<Button>
     }
 
 
+    # region LoadDialogModelCommandProperty -----------------------------
+
+    public static ICommand GetLoadDialogModelCommand(DependencyObject obj)
+    {
+        return (ICommand)obj.GetValue(LoadDialogModelCommandProperty);
+    }
+
+    public static void SetLoadDialogModelCommand(DependencyObject obj, ICommand value)
+    {
+        obj.SetValue(LoadDialogModelCommandProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for LoadDialogModelCommand.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty LoadDialogModelCommandProperty =
+        DependencyProperty.RegisterAttached(
+            "LoadDialogModelCommand",
+            typeof(ICommand),
+            typeof(DialogEventHandlerBehavior),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None, OnLoadDialogModelCommandCommandChanged));
+
+    private static void OnLoadDialogModelCommandCommandChanged(object d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not DialogEventHandlerBehavior behavior)
+        {
+            return;
+        }
+
+        if (e.NewValue is ICommand loadDialogModelCommand)
+        {
+            behavior._openedEventHandler = (sender, eventArgs) =>
+            {
+                eventArgs.Handled = true;
+                object? parameter = GetLoadDialogModelCommandParameter(behavior);
+                if (loadDialogModelCommand.CanExecute(parameter))
+                {
+                    loadDialogModelCommand.Execute(parameter);
+                }
+            };
+        }
+        else
+        {
+            behavior._openedEventHandler = (sender, eventArgs) =>
+            {
+                eventArgs.Handled = true;
+            };
+        }
+
+        DialogHost.SetDialogOpenedAttached(behavior.AssociatedObject, behavior._openedEventHandler);
+    }
+
+    #endregion --------------------------------------------------------
+
+
+    #region LoadDialogModelCommandParameterProperty -------------------
+
+    public static object? GetLoadDialogModelCommandParameter(DependencyObject obj)
+    {
+        return (object?)obj.GetValue(LoadDialogModelCommandParameterProperty);
+    }
+
+    public static void SetLoadDialogModelCommandParameter(DependencyObject obj, object? value)
+    {
+        obj.SetValue(LoadDialogModelCommandParameterProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for LoadDialogModelCommandParameter.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty LoadDialogModelCommandParameterProperty =
+        DependencyProperty.RegisterAttached(
+            "LoadDialogModelCommandParameter",
+            typeof(object),
+            typeof(DialogEventHandlerBehavior),
+            new PropertyMetadata(null));
+
+    #endregion --------------------------------------------------------
+
+
+    #region OkCommandProperty -----------------------------------------
 
     public static ICommand? GetOkCommand(DependencyObject obj)
     {
@@ -39,6 +121,10 @@ internal sealed class DialogEventHandlerBehavior : Behavior<Button>
     private static void OnOkCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         => SetCloseCommand(d, e, true);
 
+    #endregion --------------------------------------------------------
+
+
+    #region CancelCommandProperty -------------------------------------
 
     public static ICommand? GetCancelCommand(DependencyObject obj)
     {
@@ -61,7 +147,14 @@ internal sealed class DialogEventHandlerBehavior : Behavior<Button>
     public static void OnCancelCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         => SetCloseCommand(d, e, false);
 
+    #endregion --------------------------------------------------------
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="d"></param>
+    /// <param name="e"></param>
+    /// <param name="isOkCommand"></param>
     public static void SetCloseCommand(DependencyObject d, DependencyPropertyChangedEventArgs e, bool isOkCommand)
     {
         if (d is not DialogEventHandlerBehavior behavior)
@@ -111,74 +204,5 @@ internal sealed class DialogEventHandlerBehavior : Behavior<Button>
         }
 
         DialogHost.SetDialogClosedAttached(behavior.AssociatedObject, behavior._closedEventHandler);
-    }
-
-
-    public static object? GetLoadDialogModelCommandParameter(DependencyObject obj)
-    {
-        return (object?)obj.GetValue(LoadDialogModelCommandParameterProperty);
-    }
-
-    public static void SetLoadDialogModelCommandParameter(DependencyObject obj, object? value)
-    {
-        obj.SetValue(LoadDialogModelCommandParameterProperty, value);
-    }
-
-    // Using a DependencyProperty as the backing store for LoadDialogModelCommandParameter.  This enables animation, styling, binding, etc...
-    public static readonly DependencyProperty LoadDialogModelCommandParameterProperty =
-        DependencyProperty.RegisterAttached(
-            "LoadDialogModelCommandParameter",
-            typeof(object),
-            typeof(DialogEventHandlerBehavior),
-            new PropertyMetadata(null));
-
-
-
-    public static ICommand GetLoadDialogModelCommand(DependencyObject obj)
-    {
-        return (ICommand)obj.GetValue(LoadDialogModelCommandProperty);
-    }
-
-    public static void SetLoadDialogModelCommand(DependencyObject obj, ICommand value)
-    {
-        obj.SetValue(LoadDialogModelCommandProperty, value);
-    }
-
-    // Using a DependencyProperty as the backing store for LoadDialogModelCommand.  This enables animation, styling, binding, etc...
-    public static readonly DependencyProperty LoadDialogModelCommandProperty =
-        DependencyProperty.RegisterAttached(
-            "LoadDialogModelCommand",
-            typeof(ICommand),
-            typeof(DialogEventHandlerBehavior),
-            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None, OnLoadDialogModelCommandCommandChanged));
-
-    private static void OnLoadDialogModelCommandCommandChanged(object d, DependencyPropertyChangedEventArgs e)
-    {
-        if (d is not DialogEventHandlerBehavior behavior)
-        {
-            return;
-        }
-
-        if (e.NewValue is ICommand loadDialogModelCommand)
-        {
-            behavior._openedEventHandler = (sender, eventArgs) =>
-            {
-                eventArgs.Handled = true;
-                object? parameter = GetLoadDialogModelCommandParameter(behavior);
-                if (loadDialogModelCommand.CanExecute(parameter))
-                {
-                    loadDialogModelCommand.Execute(parameter);
-                }
-            };
-        }
-        else
-        {
-            behavior._openedEventHandler = (sender, eventArgs) =>
-            {
-                eventArgs.Handled = true;
-            };
-        }
-
-        DialogHost.SetDialogOpenedAttached(behavior.AssociatedObject, behavior._openedEventHandler);
     }
 }
--- src/PomodoroWindowsTimer.WpfClient/UserControls/Works/Statistic/DailyStatisticTableList.xaml.cs
+++ src/PomodoroWindowsTimer.WpfClient/UserControls/Works/Statistic/DailyStatisticTableList.xaml.cs
@@ -23,4 +23,13 @@ public partial class DailyStatisticTableList : UserControl
     {
         InitializeComponent();
     }
+
+    private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        if (sender is ScrollViewer scrollViewer)
+        {
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+    }
 }
