a766f4df0a2175555f06111c2f98e18f35a5b312
p1eXu5 <v1adp1exu5@gmail.com>
2024-08-20 16:34:39 +0300

    [ fmt ]

--- src/PomodoroWindowsTimer.WpfClient/UserControls/Works/WorkList.xaml.cs
+++ src/PomodoroWindowsTimer.WpfClient/UserControls/Works/WorkList.xaml.cs
@@ -1,42 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PomodoroWindowsTimer.WpfClient.UserControls.Works
+namespace PomodoroWindowsTimer.WpfClient.UserControls.Works;
+
+/// <summary>
+/// Interaction logic for WorkList.xaml
+/// </summary>
+public partial class WorkList : UserControl
 {
-    /// <summary>
-    /// Interaction logic for WorkList.xaml
-    /// </summary>
-    public partial class WorkList : UserControl
+    public WorkList()
     {
-        public WorkList()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            ((INotifyCollectionChanged)m_WorkList.Items).CollectionChanged += WorkList_CollectionChanged;
-        }
+        ((INotifyCollectionChanged)m_WorkList.Items).CollectionChanged += WorkList_CollectionChanged;
+    }
 
-        private void WorkList_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-        {
-            // Clear any existing sorting first
-            m_WorkList.Items.SortDescriptions.Clear();
+    private void WorkList_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        // Clear any existing sorting first
+        m_WorkList.Items.SortDescriptions.Clear();
 
-            // Sort by the Content property
-            m_WorkList.Items.SortDescriptions.Add(
-                new SortDescription(nameof(ElmishApp.WorkModel.Bindings.LastEventCreatedAt), ListSortDirection.Descending));
-        }
+        // Sort by the Content property
+        m_WorkList.Items.SortDescriptions.Add(
+            new SortDescription(nameof(ElmishApp.WorkModel.Bindings.LastEventCreatedAt), ListSortDirection.Descending));
     }
 }
