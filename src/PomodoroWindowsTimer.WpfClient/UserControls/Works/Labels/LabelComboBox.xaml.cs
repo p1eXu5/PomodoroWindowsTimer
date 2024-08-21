03737b261d31466d2e6da15d0582801ca1c613f4
p1eXu5 <v1adp1exu5@gmail.com>
2024-08-21 16:43:42 +0300

    add LabelComboBox

---
+++ src/PomodoroWindowsTimer.WpfClient/UserControls/Works/Labels/LabelComboBox.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PomodoroWindowsTimer.WpfClient.UserControls.Works.Labels;
+/// <summary>
+/// Interaction logic for LabelComboBox.xaml
+/// </summary>
+public partial class LabelComboBox : ComboBox
+{
+    public LabelComboBox()
+    {
+        InitializeComponent();
+    }
+}
