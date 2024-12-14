d530bee260c7e566c5b32912acd2ce765a42fc0a
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2024-12-14 21:18:03 +0300

    [ installer ] - git ignore set environments

--- src/PomodoroWindowsTimer.AppAgent/set_secrets_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define the paths
-ENV_FILE_PATH="$(pwd)/set_secrets.env"
-PROJECT_DIRECTORY="$(pwd)"
-
-# Run the PowerShell script
-pwsh -File "$(pwd)/set-secrets.ps1" -envFilePath "$ENV_FILE_PATH" -projectDirectory "$PROJECT_DIRECTORY"
