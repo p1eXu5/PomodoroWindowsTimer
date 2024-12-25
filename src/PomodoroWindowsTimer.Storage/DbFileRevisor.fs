adbd898209f30950890456f7648b319eacd7bca3
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2024-12-25 23:57:18 +0300

    [ chore ] - add DbFileRevisor (draft)

---
+++ src/PomodoroWindowsTimer.Storage/DbFileRevisor.fs
@@ -0,0 +1,13 @@
+﻿namespace PomodoroWindowsTimer.Storage
+
+open System.Threading
+open System.Threading.Tasks
+
+type DbFileRevisor =
+    {
+        TryUpdateDatabaseFile: unit -> CancellationToken -> Task<Result<unit, string>>
+    }
+
+module DbFileRevisor =
+    ()
+
