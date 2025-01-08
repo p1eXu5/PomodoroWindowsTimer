7abd1193bbfd7ff24ca606f5341bed412d0af314
p1eXu5 <v1adp1exu5@gmail.com>
2025-01-08 16:11:02 +0300

    fake

--- build.fsx
+++ build.fsx
@@ -119,6 +119,7 @@ let private zipFileName versionString =
 //    Targets
 // ------------------
 Target.create "CheckReleaseSecrets" (fun _ ->
+    Trace.log (sprintf "%A" Environment.environVars)
     for secret in secrets do
         secret.Force() |> ignore
 )
