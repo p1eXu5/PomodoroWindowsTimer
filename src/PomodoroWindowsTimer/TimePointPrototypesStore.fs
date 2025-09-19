99c1b941b0bee48ce0819e2ed0f48b238c143822
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2025-09-19 08:01:33 +0300

    chore: add TimePointPrototypesStore

---
+++ src/PomodoroWindowsTimer/TimePointPrototypesStore.fs
@@ -0,0 +1,34 @@
+﻿namespace PomodoroWindowsTimer
+
+open PomodoroWindowsTimer.Types
+open PomodoroWindowsTimer.Abstractions
+
+type TimePointPrototypesStore =
+    {
+        Read: unit -> TimePointPrototype list
+        Write: TimePointPrototype list -> unit
+    }
+
+module TimePointPrototypesStore =
+    let read (timePointPrototypeSettings : ITimePointPrototypesSettings) =
+        timePointPrototypeSettings.TimePointPrototypesSettings
+        |> Option.map (fun s ->
+            JsonHelpers.Deserialize<TimePointPrototype list>(s)
+        )
+        |> Option.defaultValue TimePointPrototype.defaults
+
+
+    let write (timePointPrototypeSettings : ITimePointPrototypesSettings) (timePointPrototypes: TimePointPrototype list) =
+        match timePointPrototypes with
+        | [] ->
+            timePointPrototypeSettings.TimePointPrototypesSettings <- None
+        | _ ->
+            let s = JsonHelpers.Serialize(timePointPrototypes)
+            timePointPrototypeSettings.TimePointPrototypesSettings <- s |> Some
+
+
+    let initialize (timePointPrototypeSettings : ITimePointPrototypesSettings) : TimePointPrototypesStore =
+        {
+            Read = fun () -> read timePointPrototypeSettings
+            Write = write timePointPrototypeSettings
+        }
