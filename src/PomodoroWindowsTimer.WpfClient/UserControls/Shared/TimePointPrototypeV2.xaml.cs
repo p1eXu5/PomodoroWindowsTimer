667888f5116b22bf56c6fccaf76d69e4c172c9af
Vladimir Likhatskiy <vladimir.likhatskiy@technomos.ru>
2025-09-20 14:11:25 +0300

    xaml: adjust and add dp to the TimePointPrototypeV2, adopt to the drawer TimePointsGenerator user control

--- src/PomodoroWindowsTimer.WpfClient/UserControls/Shared/TimePointPrototypeV2.xaml.cs
+++ src/PomodoroWindowsTimer.WpfClient/UserControls/Shared/TimePointPrototypeV2.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using PomodoroWindowsTimer.WpfClient.Converters;
 
 namespace PomodoroWindowsTimer.WpfClient.UserControls.Shared;
 /// <summary>
@@ -19,8 +11,63 @@ namespace PomodoroWindowsTimer.WpfClient.UserControls.Shared;
 /// </summary>
 public partial class TimePointPrototypeV2 : UserControl
 {
+    private static KindToTextConverter _kindToTextConverter;
+    private static KindToBrushConverter _kindToBrushConverter;
+
+    static TimePointPrototypeV2()
+    {
+        _kindToTextConverter = new KindToTextConverter();
+        _kindToBrushConverter = new KindToBrushConverter();
+    }
+
     public TimePointPrototypeV2()
     {
         InitializeComponent();
+
+        SetBinding(BreakWorkLabelProperty, new Binding()
+        {
+            Source = this,
+            Path = new PropertyPath(TimePointAssistant.TimePointKindProperty),
+            Mode = BindingMode.OneWay,
+            Converter = _kindToTextConverter,
+        });
+
+        SetBinding(BreakWorkBrushProperty, new Binding()
+        {
+            Source = this,
+            Path = new PropertyPath(TimePointAssistant.TimePointKindProperty),
+            Mode = BindingMode.OneWay,
+            Converter = _kindToBrushConverter,
+        });
     }
+
+    public string BreakWorkLabel
+    {
+        get { return (string)GetValue(BreakWorkLabelProperty); }
+        set { SetValue(BreakWorkLabelProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for BreakeWorkLabel.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty BreakWorkLabelProperty =
+        DependencyProperty.Register(
+            "BreakWorkLabel",
+            typeof(string),
+            typeof(TimePointPrototypeV2),
+            new PropertyMetadata(default));
+
+
+    public Brush BreakWorkBrush
+    {
+        get { return (Brush)GetValue(BreakWorkBrushProperty); }
+        set { SetValue(BreakWorkBrushProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty BreakWorkBrushProperty =
+        DependencyProperty.Register(
+            "BreakWorkBrush", 
+            typeof(Brush), 
+            typeof(TimePointPrototypeV2), 
+            new FrameworkPropertyMetadata(
+                default, FrameworkPropertyMetadataOptions.AffectsRender));
 }
