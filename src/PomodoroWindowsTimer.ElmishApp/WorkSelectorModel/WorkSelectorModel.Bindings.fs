15a106493792db96903cceef0603e91ac5824d21
p1eXu5 <v1adp1exu5@gmail.com>
2025-09-23 10:03:25 +0300

    ref: replace Bindings class with bindings list

--- src/PomodoroWindowsTimer.ElmishApp/WorkSelectorModel/WorkSelectorModel.Bindings.fs
+++ src/PomodoroWindowsTimer.ElmishApp/WorkSelectorModel/WorkSelectorModel.Bindings.fs
@@ -7,42 +7,34 @@ open PomodoroWindowsTimer.ElmishApp
 open PomodoroWindowsTimer.ElmishApp.Models
 open PomodoroWindowsTimer.ElmishApp.Models.WorkSelectorModel
 
-type private Binding = Binding<WorkSelectorModel, WorkSelectorModel.Msg>
+type IBindings =
+    interface
+        abstract SubModelId: SubModelId
+        abstract WorkListModel: WorkListModel.IBindings option
+        abstract CreatingWorkModel: CreatingWorkModel.IBindings option
+        abstract UpdatingWorkModel: WorkModel.IBindings option
+    end
 
-[<Sealed>]
-type Bindings() =
-    static let props = Utils.bindingProperties typeof<Bindings>
-    static let mutable __ = Unchecked.defaultof<Bindings>
-    static member Instance() =
-        if System.Object.ReferenceEquals(__, null) then
-            __ <- Bindings()
-            __
-        else __
+module Bindings =
+    let private __ = Unchecked.defaultof<IBindings>
 
-    static member ToList () =
-        Utils.bindings<Binding>
-            (Bindings.Instance())
-            props
+    let bindings () =
+        [
+            nameof __.SubModelId |> Binding.oneWay subModelId
 
-    member val SubModelId : Binding =
-        nameof __.SubModelId |> Binding.oneWay subModelId
+            nameof __.WorkListModel
+                |> Binding.SubModel.opt WorkListModel.Bindings.bindings
+                |> Binding.mapModel workListModel
+                |> Binding.mapMsg Msg.WorkListModelMsg
 
-    member val WorkListModel : Binding =
-        nameof __.WorkListModel
-            |> Binding.SubModel.opt WorkListModel.Bindings.ToList
-            |> Binding.mapModel workListModel
-            |> Binding.mapMsg Msg.WorkListModelMsg
+            nameof __.CreatingWorkModel
+                |> Binding.SubModel.opt CreatingWorkModel.Bindings.bindings
+                |> Binding.mapModel creatingWorkModel
+                |> Binding.mapMsg Msg.CreatingWorkModelMsg
 
-    member val CreatingWorkModel : Binding =
-        nameof __.CreatingWorkModel
-            |> Binding.SubModel.opt CreatingWorkModel.Bindings.ToList
-            |> Binding.mapModel creatingWorkModel
-            |> Binding.mapMsg Msg.CreatingWorkModelMsg
-
-    member val UpdatingWorkModel : Binding =
-        nameof __.UpdatingWorkModel
-            |> Binding.SubModel.opt WorkModel.Bindings.bindings
-            |> Binding.mapModel updatingWorkModel
-            |> Binding.mapMsg Msg.UpdatingWorkModelMsg
-
+            nameof __.UpdatingWorkModel
+                |> Binding.SubModel.opt WorkModel.Bindings.bindings
+                |> Binding.mapModel updatingWorkModel
+                |> Binding.mapMsg Msg.UpdatingWorkModelMsg
+        ]
 
